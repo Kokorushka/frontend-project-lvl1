b9aeca944c2ab53b45545799be587365c0b7235d
Alexandra <kokosha117@gmail.com>
2020-11-26 11:05:07 +0300

    change brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,4 @@
 
 import getAndPrintName from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
 getAndPrintName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from '../node_modules/readline-sync/lib/readline-sync.js';
 
 const getAndPrintName = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
